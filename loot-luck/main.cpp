c5b509383dd1114df4b25519fa51fbb1465ee295
yano <yano@liberogic.jp>
2018-02-21 21:15:35 +0900

    適当

--- loot-luck/main.cpp
+++ loot-luck/main.cpp
@@ -18,8 +18,8 @@
 #define MIN_LOOT    (1)
 #define MAX_LOOT    (99)
 
-enum State {PASS = 0, NEED, GREED};
-const char *StateStr[] = {"Pass", "Neeed", "Greed"};
+enum State {PASS = 0, GREED, NEED};
+const char *StateStr[] = {"Pass", "Greed", "Need"};
 
 typedef struct Party {
     int pos;
@@ -27,46 +27,14 @@ typedef struct Party {
     int state;
 }party_t;
 
-void drawing_lot(int mode, const char *item, int max_party = 4) {
-    std::random_device dev;
-    std::mt19937 generator(dev());
-    
-    std::uniform_int_distribution<> st(0,2);
-    std::vector<party_t> party_list;
-    for (int a = 0; a < max_party; ++a) {
-        int state = st(generator);
-        party_t p = {0, 0};
-        p.pos = a;
-        p.lot = 0;
-        p.state = (a == 0) ? mode : state;
-        party_list.push_back(p);
-        //free(p);
-    }
-    
-    std::uniform_int_distribution<> lots(MIN_LOOT, MAX_LOOT);
+/**
+ * ロット
+ * @param mode - ロットタイプ
+ * @param item - アイテム名
+ * @param max_party - パーティ人数
+ */
+void drawing_loot(int mode, const char *item, int max_party = 4);
 
-    for (auto &pt: party_list) {
-        int lot = lots(generator);
-        pt.lot = lot;
-        if (pt.state == 0) { pt.lot = 0; }
-        if (pt.pos == 0) {
-            std::cout << BOLDCYAN << "[*]";
-        } else {
-            std::cout << RESET << "   ";
-        }
-        
-        std::cout << "P" << (pt.pos + 1) << "は" << item << "に" << StateStr[pt.state] << "のダイスで" << lot << "を出した。" << '\n';
-        //std::cout << "P" << (pt.pos + 1) << ": " << lot << " [" << (pt.state) << "]" << '\n';
-    }
-    
-    /**
-     * Get max element in a vector of structures in c++
-     * @see - https://stackoverflow.com/questions/16772477/how-to-get-min-or-max-element-in-a-vector-of-structures-in-c-based-on-some-fi
-     */
-    std::vector<party_t>::iterator itr;
-    itr = std::max_element(party_list.begin(), party_list.end(), [](party_t &p1, party_t &p2) { return p1.lot < p2.lot; });
-    std::cout << "P" << (itr->pos + 1) << "は" << item << "を手に入れた。" << '\n';
-}
 
 int main(int argc, const char * argv[]) {
     char mode;
@@ -90,10 +58,57 @@ int main(int argc, const char * argv[]) {
     choose['N'] = NEED;
     choose['G'] = GREED;
     choose['P'] = PASS;
-    drawing_lot(choose[mode], "アイテム", 8);
+    drawing_loot(choose[mode], "アイテム", 8);
     
     end = std::chrono::system_clock::now();
     double elapsed = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     std::cout << "処理計測: " << elapsed << " ms" << '\n';
     return 0;
 }
+
+void drawing_loot(int mode, const char *item, int max_party) {
+    std::random_device dev;
+    std::mt19937 generator(dev());
+    
+    std::uniform_int_distribution<> st(0,2);
+    std::vector<party_t> party_list;
+    for (int a = 0; a < max_party; ++a) {
+        int state = st(generator);
+        party_t p = {0, 0};
+        p.pos = a;
+        p.lot = 0;
+        p.state = (a == 0) ? mode : state;
+        party_list.push_back(p);
+    }
+    
+    std::uniform_int_distribution<> lots(MIN_LOOT, MAX_LOOT);
+    
+    for (auto &pt: party_list) {
+        int lot = lots(generator);
+        pt.lot = lot;
+        if (pt.state == PASS) { pt.lot = 0; }
+        if (pt.pos == 0) {
+            std::cout << BOLDCYAN << "[*]";
+        } else {
+            std::cout << RESET << "   ";
+        }
+        
+        std::cout << "P" << (pt.pos + 1) << "は" << item;
+        if (pt.state == PASS) {
+            std::cout << "のロットをパスした。\n";
+        } else {
+            std::cout << "に" << StateStr[pt.state] << "のダイスで" << pt.lot << "を出した。" << '\n';
+        }
+    }
+    
+    /**
+     * Get max element in a vector of structures in c++
+     * @see - https://stackoverflow.com/questions/16772477/how-to-get-min-or-max-element-in-a-vector-of-structures-in-c-based-on-some-fi
+     */
+    std::vector<party_t>::iterator itr;
+    itr = std::max_element(party_list.begin(), party_list.end(), [](party_t &p1, party_t &p2) {
+        return p1.lot < p2.lot;
+    });
+    std::cout << "P" << (itr->pos + 1) << "は" << item << "を手に入れた。" << '\n';
+}
+
